e6e746df8ae9821c220ef59ffd44f965844ad7db
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-25 16:37:34 -0400

    Remove hardcoded and placeholder content

--- assets/scripts/display.js
+++ assets/scripts/display.js
@@ -18,33 +18,6 @@ if (yelpFetch){
     });
 }
 
-
-//just using the first restaurant of the object, will need a for loop for all restaurants, need to find a way to generate number of sections accordingly to search result
-document.getElementById('image').setAttribute('src','https://s3-media1.fl.yelpcdn.com/bphoto/iD1cCbTASjC7YOb3BZAWDw/o.jpg')
-document.getElementById('name').textContent=info[0].name
-document.getElementById('name').setAttribute('href',`${info[0].url}`) //this doesn't work yet
-document.getElementById('phone').textContent=info[0].display_phone
-document.getElementById('type').textContent=info[0].categories[0].alias
-let transaction = document.getElementById('transaction')
-if (info[0].transactions.length === 0){
-    transaction.textContent = 'Dine in only'
-} else{
-    for (way in transaction){
-        document.getElementById('transaction').textContent= way + ' '
-    }
-}
-document.getElementById('distance').textContent=(parseInt(info[0].distance)/1000).toFixed(1).toString() + ' km'
-let rating = info[0].rating
-document.getElementById('rating').setAttribute('src',`./assets/images/large_${Math.floor(rating)}.png`)
-document.getElementById('review').textContent=info[0].review_count + " reviews"
-if (document.getElementById('is-closed')){
-    document.getElementById('is-closed').textContent='Closed'
-}else if (!(document.getElementById('is-closed'))){
-    document.getElementById('is-closed').textContent='Open'
-}
-document.getElementById('price').textContent=info[0].price || '$?'
-//Not all restaurants contain a expensive rating
-
 function createDisplayCard(restaurantObj) {
     const restaurantContent = createCardContent(restaurantObj)
     const restaurantFooter = createCardFooter(restaurantObj)
