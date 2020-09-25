f139d434865b4b2408e5539482d55b410780fb86
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-25 15:13:36 -0400

    Remove global variables

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,44 +1,50 @@
-let latitude
-let longitude
-let searchInput = document.getElementById('searchTerm')
-let searchForm = document.getElementById('searchForm')
-let category = document.getElementById('categoryButtons')
-
+//ensure user accepts to get location first
 navigator.geolocation.getCurrentPosition(init)
 
 function init(location){
-    searchForm.addEventListener('submit',handleSubmit)
-    category.addEventListener('click',searchCategory)
-    latitude = location.coords.latitude.toFixed(6)
-    longitude = location.coords.longitude.toFixed(6)
+    const latitude = location.coords.latitude.toFixed(6)
+    const longitude = location.coords.longitude.toFixed(6)
+
+    const searchInput = document.getElementById('searchTerm')
+    const searchForm = document.getElementById('searchForm')
+    searchForm.addEventListener('submit', function(event) {
+        handleSubmit(event, searchInput.value, latitude, longitude)
+    })
+
+    const category = document.getElementById('categoryButtons')
+    if (category) {
+        category.addEventListener('click', function(event) {
+            searchCategory(event, longitude, latitude)
+        })
+    }
 }
 
-function handleSubmit(event){
+function handleSubmit(event, searchInput, latitude, longitude){
     event.preventDefault()
-    searching(searchInput.value)
+    searching(searchInput, latitude, longitude)
 }
 
-function searching(searchTerm){
-    fetchRestaurants(searchTerm)
+function searching(searchTerm, latitude, longitude){
+    fetchRestaurants(searchTerm, latitude, longitude)
     //direct to listing page
 }
 
-function searchCategory(event){
+function searchCategory(event, longitude, latitude){
     if (event.target.matches('.category')){
         searchTerm = event.target.textContent.trim()
-        searching(searchTerm)
+        searching(searchTerm, latitude, longitude)
     }
 
 }
 
-function fetchRestaurants (searchTerm){
+function fetchRestaurants (searchTerm, latitude, longitude){
     const YELP_API_KEY =
         'NdXFjJC2Z5-HgDBjoNaueLO36hrPTyedCA-GNKcoSl6oYQV6qiIkWXI89HGaxlrEoBNYFt0CFYeiMhyb8rCYqUqWfbF0-qRXgBJOYHirnXdPjl2kjj_3w-6MySlmX3Yx'
     const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
     const BASE_URL = 'https://api.yelp.com/v3/businesses'
     const geoCoords = `latitude=${latitude}&longitude=${longitude}`
     const url = `${CORS_PROXY}${BASE_URL}/search?radius=10000&categories=restaurants&term=${searchTerm}&${geoCoords}`
-    
+
     fetch(url, {
     headers: {
         'Content-Type': 'application/json',
